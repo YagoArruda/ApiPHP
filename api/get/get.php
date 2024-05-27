d897501e969c28998cc52d8b80eb55887acdd807
YagoArruda <yago4444@gmail.com>
2024-05-27 13:02:29 -0300

    Atualização de rota 2

--- api/get/get.php
+++ api/get/get.php
@@ -1,57 +1,37 @@
 <?php
-header('Content-Type: application/json');
-
-$servername = 'srv1197.hstgr.io'; 
+// Dados de conexão com o banco de dados
+$servername = 'srv1197.hstgr.io';
 $username = 'u689582486_user';
 $password = '5e^TKn5ISqX';
 $dbname = 'u689582486_teste';
 
-$resultadoJSON = array();
-
-// Cria conexão
-$mysqli = new mysqli($servername, $username, $password, $dbname);
+// Conexão com o banco de dados
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verifica a conexão
-if ($mysqli->connect_errno) {   
-    echo "Falha na conexão: " . $mysqli->connect_errno . " / " . $mysqli->connect_error;
-    exit();
+if ($conn->connect_error) {
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 
-// Executa a consulta
-$query = "SELECT * FROM livros";
-$result = $mysqli->query($query);
+// Verifica se o método de requisição é GET
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    // Executa a query para selecionar os dados
+    $sql = "SELECT * FROM livros";
+    $result = $conn->query($sql);
 
-// Verifica se a consulta foi bem-sucedida
-if (!$result) {
-    echo "Erro na consulta: " . $mysqli->error;
-    exit();
-}
-
-// Verifica se existem linhas retornadas
-if ($result->num_rows > 0) {
-    // Itera sobre as linhas retornadas e exibe os dados
-    while ($row = $result->fetch_assoc()) {
-        // Adiciona os dados de cada linha como um array associativo ao array $resultadoJSON
-        $livro = array(
-            "id" => $row["id_livro"],
-            "nome" => $row["nome"],
-            "autor" => $row["autor"],
-            "resumo" => $row["resumo"],
-            "genero" => $row["genero"]
-        );
-        // Adiciona o array do livro ao array $resultadoJSON
-        $resultadoJSON[] = $livro;
+    if ($result->num_rows > 0) {
+        $livros = array();
+        while($row = $result->fetch_assoc()) {
+            $livros[] = $row;
+        }
+        echo json_encode($livros);
+    } else {
+        echo "Nenhum livro encontrado.";
     }
-} 
-else {
-    echo "Nenhum resultado encontrado.";
+} else {
+    echo "Método de requisição inválido. Use GET.";
 }
 
 // Fecha a conexão
-$mysqli->close();
-
-// Fazendo echo dos dados do livro como JSON
-//echo json_encode($livro);
-
-echo json_encode($resultadoJSON);
-
+$conn->close();
+?>
